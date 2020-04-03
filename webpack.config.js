8a1d6a01402b9365fb679b0df2409c4f78023704
Eli <eli.i.pleaner@gmail.com>
2020-04-03 17:03:20 +0200

    Add webpack + babel

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-env"]
+        }
+      }
+    ]
+  }
+};
