95091812a28a06f4d0edeff5fe21f330e6b9ceb3
Eli <eli.i.pleaner@gmail.com>
2020-05-07 16:08:45 +0200

    Config webpack to support css and image assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,44 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  devtool: "eval-source-map",
+  devtool: 'eval-source-map',
   resolve: {
     alias: {
-      Mesh: path.resolve(__dirname, "src/mesh/"),
-      Camera: path.resolve(__dirname, "src/camera/"),
-      Scene: path.resolve(__dirname, "src/scene/"),
-      Canvas: path.resolve(__dirname, "src/canvas/")
+      Mesh: path.resolve(__dirname, 'src/mesh/'),
+      Camera: path.resolve(__dirname, 'src/camera/'),
+      Scene: path.resolve(__dirname, 'src/scene/'),
+      Canvas: path.resolve(__dirname, 'src/canvas/'),
+      '@textures': path.resolve(__dirname, 'src/assets/textures/'),
     },
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-env"]
-        }
-      }
-    ]
-  }
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+            { plugins: ['@babel/plugin-proposal-class-properties'] },
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
 };
