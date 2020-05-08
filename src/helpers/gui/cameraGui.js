6fbe20b8c1457e20ebc199ef7ccacfa06d8616be
Eli <eli.i.pleaner@gmail.com>
2020-05-08 16:22:06 +0200

    Camera GUI generator

---
+++ src/helpers/gui/cameraGui.js
@@ -0,0 +1,16 @@
+import MinMaxGuiHelper from '@helpers/gui/minMaxGuiHelper';
+
+export const cameraGui = ({ gui, camera }) => {
+  camera.fov && gui.add(camera, 'fov', 1, 180).onChange(camera.update);
+  const minMaxGuiHelper = new MinMaxGuiHelper(camera, 'near', 'far', 0.1);
+  gui
+    .add(minMaxGuiHelper, 'min', 0.1, 50, 0.1)
+    .name('near')
+    .onChange(camera.update);
+  gui
+    .add(minMaxGuiHelper, 'max', 0.1, 50, 0.1)
+    .name('far')
+    .onChange(camera.update);
+
+  camera.zoom && gui.add(camera, 'zoom', 0.01, 1, 0.01).listen();
+};
