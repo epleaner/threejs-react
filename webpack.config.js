92d5e355a0bb9e4b3567dda4ff2cc606b69e5be0
Eli <eli.i.pleaner@gmail.com>
2020-05-08 14:10:05 +0200

    Update webpack config with clean-plugin & aliases

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,10 +14,25 @@ module.exports = {
       Camera: path.resolve(__dirname, 'src/camera/'),
       Scene: path.resolve(__dirname, 'src/scene/'),
       Canvas: path.resolve(__dirname, 'src/canvas/'),
+
+      '@three': path.resolve('node_modules/three/build/three.module.js'),
+      '@three-controls': path.resolve(
+        'node_modules/three/examples/jsm/controls'
+      ),
+      '@three-libs': path.resolve('node_modules/three/examples/jsm/libs'),
+      '@three-gui': path.resolve(
+        'node_modules/three/examples/jsm/libs/dat.gui.module.js'
+      ),
+      '@three-modules': path.resolve('node_modules/three/examples/jsm'),
+
       '@textures': path.resolve(__dirname, 'src/assets/textures/'),
+      '@styles': path.resolve(__dirname, 'src/assets/styles/'),
+      '@helpers': path.resolve(__dirname, 'src/helpers/'),
+      '@factories': path.resolve(__dirname, 'src/helpers/factories/'),
     },
     extensions: ['.js', '.jsx'],
   },
+  plugins: [new CleanWebpackPlugin()],
   module: {
     rules: [
       {
