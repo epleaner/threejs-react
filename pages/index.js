d7ab1820474bb10b6351f0f90eb83207ae09f158
Eli <eli.i.pleaner@gmail.com>
2020-05-21 16:09:20 +0200

    Styling index

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 const Index = () => {
   return (
-    <main className='center-children h-screen grid grid-cols-1 lg:grid-cols-2'>
-      <section className='grid grid-rows-2'>
+    <main className='bg-gray-900 h-screen grid grid-cols-1 lg:grid-cols-2'>
+      <section className='lg:mx-10 mx-4'>
         <h1 className='text-6xl text-gray-400'>3D</h1>
-        <nav className='center-children my-4'>
+        <nav className='grid grid-cols-2 my-4'>
           <Link href='/organism'>
             <a className='card grow'>Organism</a>
           </Link>
@@ -15,11 +15,14 @@ const Index = () => {
           </Link>
         </nav>
       </section>
-      <section className='center-children grid grid-rows-2'>
+      <section className='lg:mx-10 mx-4'>
         <h1 className='text-6xl text-gray-400'>2D</h1>
-        <nav className='center-children my-4'>
+        <nav className='grid grid-cols-2 my-4'>
           <Link href='/sandstone'>
-            <a className='card grow'>Sandstone</a>
+            <a className='card grow'>Sandstone 1</a>
+          </Link>
+          <Link href='/sandstone-yarn'>
+            <a className='card grow'>Sandstone Yarn</a>
           </Link>
           <Link href='/waves'>
             <a className='card grow'>Waves</a>
