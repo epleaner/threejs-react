d554adbec8fdd33cc56af39f26d86e2fce9ebba1
Eli <eliipleaner@gmail.com>
2020-06-09 20:35:05 +0200

    Add first person controls

--- src/components/three/controls.js
+++ src/components/three/controls.js
@@ -1,9 +1,10 @@
 import React, { useRef } from 'react';
 import { extend, useFrame, useThree } from 'react-three-fiber';
+import { FirstPersonControls } from '@three-controls/FirstPersonControls.js';
 import { OrbitControls } from '@three-controls/OrbitControls.js';
-extend({ OrbitControls });
+extend({ OrbitControls, FirstPersonControls });
 
-export default function Controls() {
+export const Orbit = () => {
   const controls = useRef();
   const { camera, gl } = useThree();
   useFrame(() => controls.current.update());
@@ -12,8 +13,27 @@ export default function Controls() {
       ref={controls}
       args={[camera, gl.domElement]}
       enableDamping
-      dampingFactor={0.1}
+      dampingFactor={0.05}
       rotateSpeed={0.5}
     />
   );
-}
+};
+
+export const FirstPerson = () => {
+  const controls = useRef();
+  const { camera, gl } = useThree();
+
+  useFrame((state) => {
+    const delta = state.clock.getDelta();
+    controls.current.update(delta);
+  });
+
+  return (
+    <firstPersonControls
+      ref={controls}
+      args={[camera, gl.domElement]}
+      movementSpeed={1000}
+      lookSpeed={1}
+    />
+  );
+};
