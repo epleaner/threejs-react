33f35b5cd50c4699653699b37e8aa5e5408659a4
Eli <eliipleaner@gmail.com>
2020-06-10 10:55:41 +0200

    Playing with stars

--- pages/pacific.js
+++ pages/pacific.js
@@ -2,35 +2,27 @@ import dynamic from 'next/dynamic';
 import React, { Suspense } from 'react';
 import { Canvas } from 'react-three-fiber';
 import { Controls as GUI, useControl } from 'react-three-gui';
-import { Sky } from 'drei';
+import { Sky, Stars } from 'drei';
 import Ocean from '@components/three/ocean';
 import { FirstPerson as FirstPersonControls } from '@components/three/controls';
 
 const Page = () => {
-  const turbidity = useControl('turbidity', {
-    type: 'number',
-    value: 10,
-    min: 1,
-    max: 20,
-  });
-
-  const rayleigh = useControl('rayleigh', {
-    type: 'number',
-    value: 2,
-    min: 0,
-    max: 4,
+  const stars = useControl('stars', {
+    type: 'boolean',
   });
 
   return (
     <>
       <Canvas camera={{ fov: 60, near: 1, far: 20000, position: [0, 100, 0] }}>
-        <Sky {...{ turbidity, rayleigh }} />
         <Suspense fallback={null}>
           <Ocean />
+          <Sky />
+          {stars && <Stars />}
         </Suspense>
         <fog attach='fog' args={[0xaaccff, 0.0007]} />
         <FirstPersonControls />
       </Canvas>
+      <GUI />
     </>
   );
 };
