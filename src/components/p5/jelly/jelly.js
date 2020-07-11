c7c54540a9ea0141c376427573a91f427c6bfad1
Eli <eliipleaner@gmail.com>
2020-07-11 13:57:59 +0200

    WIP: jellyfish project

--- src/components/p5/jelly/jelly.js
+++ src/components/p5/jelly/jelly.js
@@ -1,43 +1,52 @@
 export default class Jelly {
   offset = 0;
-  length = 50;
+  amplitude = 4;
+  length = 25;
   rotation = 0;
   topSpeed = 5;
+  rotation;
   p5;
   location;
   velocity;
-  acceleration;
 
   constructor(p5) {
     this.p5 = p5;
     this.location = p5.createVector(0, 0);
     this.velocity = p5.createVector(0, 0);
-    this.acceleration = p5.createVector(-0.001, 0.1);
   }
 
-  init() {}
-
   update() {
-    const mouseV = this.p5.createVector(this.p5.mouseX, this.p5.mouseY);
-    mouseV.sub(this.location);
-    mouseV.normalize();
-    mouseV.mult(0.5);
-    this.velocity.add(mouseV);
+    const targetV = this.p5.createVector(
+      this.p5.mouseX - this.p5.width / 2,
+      this.p5.mouseY - this.p5.height / 2
+    );
+
+    targetV.sub(this.location);
+    targetV.normalize();
+    targetV.mult(0.001);
+
+    this.velocity.add(targetV);
     this.velocity.limit(this.topSpeed);
     this.location.add(this.velocity);
-    this.p5.translate(this.location.x, this.location.y);
-    // this.p5.rotate(Math.PI * this.rotation);
+
+    this.rotation = targetV.angleBetween(this.location);
   }
 
   draw() {
-    this.p5.noFill();
-    this.p5.stroke(0);
+    this.p5.push();
 
-    let len = 0;
-    while (len < this.length) {
-      this.p5.point(len, 5 * Math.sin((len + this.offset) / 10));
-      len += 0.01;
+    this.p5.translate(this.location.x, this.location.y);
+    this.p5.rotate(-this.rotation);
+    this.p5.stroke(2);
+
+    let x = 0;
+
+    while (x <= this.length) {
+      this.p5.point(x, this.amplitude * Math.sin((x + this.offset) / 10));
+      x += 1;
     }
+
+    this.p5.pop();
 
     this.offset++;
   }
